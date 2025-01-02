57e1abcf2ea18086e0a5f3fb0722ece4168083d1
himanshuverma13 <himanshu.verma@skillintelligence.tech>
2025-01-02 12:13:48 +0530

    first commit 2/1/25

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='flex'>
+    <h1 className="text-4xl font-bold text-red-600">
+         lets start Working
+      </h1>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
