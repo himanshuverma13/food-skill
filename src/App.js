858d6ca54e563e60db2b0f791496b9b3dd2bfea3
ShumailaKhan12 <shumaila1641@gmail.com>
2025-01-06 11:47:40 +0530

    Working in login page 06-01-25

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import RightSidebar from './Components/Common/SideNavbar/rightSideNavbar';
+import UserLogin from './Components/Common/UserLogin/userlogin';
 
 function App() {
   return (
     <>
-    <div className=''>
+    <div className='background-img'>
+    <UserLogin/>
     <RightSidebar/>
       
     </div>
