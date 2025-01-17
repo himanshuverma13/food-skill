5c7ff91dab4c4b2316e5ddfa7ddd3073ebc448ad
himanshuverma13 <himanshu.verma@skillintelligence.tech>
2025-01-17 17:18:30 +0530

    working on table page table 17/1/25

--- src/Components/Common/MenuCategorySlider/MenuSlider.jsx
+++ src/Components/Common/MenuCategorySlider/MenuSlider.jsx
@@ -14,7 +14,7 @@ const MenuSlider = () => {
         speed: 500,
         // autoplay: true,
         // autoplaySpeed: 1000,
-        slidesToShow: 9,
+        slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
             {
