1e320cfe1ace8daf9af8b1ce4548c66e4f18cbdc
ShumailaKhan12 <shumaila1641@gmail.com>
2025-01-17 17:25:53 +0530

    Working in slider & cards 17-1-25

--- src/Components/Common/RouteRoutes/routes.jsx
+++ src/Components/Common/RouteRoutes/routes.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../../Pages/Home/home";
 import UserLogin from "../../Common/UserLogin/userlogin";
 import Menu from "../../Pages/Menu/menu";
+import Table from "../../Pages/Table/table";
+import PreviousOrder from "../../Pages/PreviousOrder/previousOrder";
 const RouteRoutes = () => {
     return (
         <>
@@ -11,6 +13,10 @@ const RouteRoutes = () => {
                     <Route path="/" element={<UserLogin />}></Route>
                     <Route path="/home" element={<Home />}></Route>
                     <Route path="/menu" element={<Menu />}></Route>
+                    <Route path="/table" element={<Table />}></Route>
+                    <Route path="/previousorder" element={<PreviousOrder />}></Route>
+
+
 
 
                 </Routes>
