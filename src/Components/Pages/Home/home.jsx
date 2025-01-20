5b6ca4517fe4629dec2570dc9668268265e4e223
ShumailaKhan12 <shumaila1641@gmail.com>
2025-01-20 17:42:49 +0530

    Working in order &previous page 20-1-25

--- src/Components/Pages/Home/home.jsx
+++ src/Components/Pages/Home/home.jsx
@@ -20,64 +20,63 @@ const Home = () => {
 
       {/* Main Content Area */}
       <div className={`flex-grow p-4 transition-all duration-300`}>
-        <Navbar/>
+        <Navbar />
         {/* -------- for 2 member table ---------- */}
-        <hr className="mt-3 mb-1"/>
-        <h2 className="text-base font-semibold">Table for 2 members</h2>
-        <div
-          className={`grid ${
-            isRightSidebarOpen == true ? "grid-cols-4" : "grid-cols-6"
-          } gap-4`}
-        >
-          {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
-            <>
-              {/* <div key={i} onClick={toggleRightSidebar} className="bg-white rounded-lg shadow-md p-4 ">
+        <hr className="mt-3 mb-1" />
+
+        <div className="overflow-auto h-full order-card-scroll">
+          <h2 className="text-base font-semibold">Table for 2 members</h2>
+          <div
+            className={`grid ${isRightSidebarOpen == true ? "md:grid-cols-2 lg:grid-cols-4" : "md:grid-cols-3 lg:grid-cols-6"
+              } gap-4`}
+          >
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
+              <>
+                {/* <div key={i} onClick={toggleRightSidebar} className="bg-white rounded-lg shadow-md p-4 ">
               <h2>Card {i}</h2>
               <p>Some content for card {i}</p>
             </div> */}
-              <TableCard />
-            </>
-          ))}
-        </div>
-        {/* -------- for 4 member table ---------- */}
-        <hr className="mt-3 mb-1" />
-        <h2 className="text-base font-semibold">Table for 4 members</h2>
-        <div
-          className={`grid ${
-            isRightSidebarOpen == true ? "grid-cols-4" : "grid-cols-6"
-          } gap-4`}
-        >
-          {[1, 2, 3, 4, 5, 6].map((i) => (
-            <>
-              <TableCard />
-            </>
-          ))}
-        </div>
-        {/* -------- for family member table ---------- */}
-        <hr className="mt-3 mb-1" />
-        <div className="flex justify-between">
-        <span className="text-base font-semibold">Table for family members</span>
-        <span className="text-sm font-light ">Max Capacity 12 members</span>
+                <TableCard />
+              </>
+            ))}
+          </div>
+          {/* -------- for 4 member table ---------- */}
+          <hr className="mt-3 mb-1" />
+          <h2 className="text-base font-semibold">Table for 4 members</h2>
+          <div
+            className={`grid ${isRightSidebarOpen == true ? "md:grid-cols-2 lg:grid-cols-4" : "md:grid-cols-3 lg:grid-cols-6"
+              } gap-4`}
+          >
+            {[1, 2, 3, 4, 5, 6].map((i) => (
+              <>
+                <TableCard />
+              </>
+            ))}
+          </div>
+          {/* -------- for family member table ---------- */}
+          <hr className="mt-3 mb-1" />
+          <div className="flex justify-between">
+            <span className="text-base font-semibold">Table for family members</span>
+            <span className="text-sm font-light ">Max Capacity 12 members</span>
 
-        </div>
-        <div
-          className={`grid ${
-            isRightSidebarOpen == true ? "grid-cols-4" : "grid-cols-6"
-          } gap-4`}
-        >
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => (
-            <>
-              <TableCard />
-            </>
-          ))}
+          </div>
+          <div
+            className={`grid ${isRightSidebarOpen == true ? "md:grid-cols-2 lg:grid-cols-4" : "md:grid-cols-3 lg:grid-cols-6"
+              } gap-4`}
+          >
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => (
+              <>
+                <TableCard />
+              </>
+            ))}
+          </div>
         </div>
       </div>
 
       {/* Right Sidebar */}
       <div
-        className={`bg-gray-200 transition-all duration-300 ease-in-out relative rounded-l-3xl ${
-          isRightSidebarOpen ? "w-80" : "w-9"
-        }`}
+        className={`bg-gray-200 transition-all duration-300 ease-in-out relative rounded-l-3xl ${isRightSidebarOpen ? "w-80" : "w-9"
+          }`}
       >
         {/* <button
             onClick={toggleRightSidebar}
